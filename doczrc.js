8d556fac21ae1627d1b76d670ebd7d4433e6d0b4
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-05-04 20:20:37 +1000

    chore: Setup docz, enable CSS and order the menu

---
+++ doczrc.js
@@ -0,0 +1,19 @@
+import { css } from 'docz-plugin-css';
+
+export default {
+  title: 'Window-Table',
+  description: 'Render millions of rows in a table',
+  files: 'docz/**/*.mdx',
+  dest: '/website',
+  plugins: [
+    css({
+      preprocessor: 'sass'
+    })
+  ],
+  menu: [
+    { name: 'Getting Started', menu: ['Introduction', 'Basic Usage'] },
+    { name: 'Examples', menu: ['Custom Cell Renderer'] },
+    { name: 'Components API', menu: ['WindowTable', 'HTML5Table'] },
+    { name: 'Github', href: 'https://github.com/pupudu/window-table' }
+  ]
+};
