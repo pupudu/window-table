26a3bd26fff9b934a0b83cea58550c7e0a7c5c8b
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-05-07 15:45:20 +1000

    fix: Use empty string when className is undefined

--- docz/bootstrap.js
+++ docz/bootstrap.js
@@ -2,5 +2,7 @@ import React from 'react';
 import './bootstrap.scss';
 
 export default function Bootstrap(props) {
-  return <div className={`${props.className} bootstrap-wrapper`} {...props} />;
+  return (
+    <div className={`${props.className || ''} bootstrap-wrapper`} {...props} />
+  );
 }
